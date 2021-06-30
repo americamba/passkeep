c733f9ee4b3b665b5feca46914b5865cda2e5482
americamba <americamba@gmail.com>
2021-06-30 16:58:36 -0500

    Removed key used for local dev.

--- passkeep/settings.py
+++ passkeep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s%mw+p2ptnp%+ax+fq&)d8p3^8+u$myqg1kz7^(@_t-1h8zp_n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
